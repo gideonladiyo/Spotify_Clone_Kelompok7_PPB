d70b344d2ebd887af2bc2716cad78770b344af98
Gideon Ladiyo <alwaysfun0604@gmail.com>
2024-11-21 10:06:12 +0700

    fix profile ui

--- lib/presentation/profile/profile.dart
+++ lib/presentation/profile/profile.dart
@@ -22,141 +22,161 @@ class Profile extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          // Profile Header
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 30),
-            decoration: BoxDecoration(
-              color: AppColors.secondaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Profile Header
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 30),
+              decoration: BoxDecoration(
+                color: AppColors.secondaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.grey,
+                    child: Icon(Icons.person, size: 40, color: Colors.white),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Kelompok 7",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "kelompokpbp7@gmail.com",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Text("Followers", style: TextStyle(fontSize: 12)),
+                          SizedBox(height: 5),
+                          Text("5",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20)),
+                        ],
+                      ),
+                      SizedBox(width: 100),
+                      Column(
+                        children: [
+                          Text("Following", style: TextStyle(fontSize: 12)),
+                          SizedBox(height: 5),
+                          Text("5",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person, size: 40, color: Colors.white),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  "Kelompok 7",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  "kelompokpbp7@gmail.com",
-                  style: TextStyle(fontSize: 14),
-                ),
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      children: [
-                        Text("Followers", style: TextStyle(fontSize: 12)),
-                        SizedBox(height: 5),
-                        Text("5",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20)),
-                      ],
-                    ),
-                    SizedBox(width: 100),
-                    Column(
-                      children: [
-                        Text("Following", style: TextStyle(fontSize: 12)),
-                        SizedBox(height: 5),
-                        Text("5",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20)),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
 
-          SizedBox(height: 10),
+            SizedBox(height: 10),
 
-          // Find Friend and Share Button
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 15),
-            decoration: BoxDecoration(
-              color: AppColors.darkBG,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.white.withOpacity(0.2),
-                  blurRadius: 3,
-                  offset: Offset(0, 5),
-                ),
-              ],
+            // Find Friend and Share Button
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 15),
+              decoration: BoxDecoration(
+                color: AppColors.darkBG,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.white.withOpacity(0.2),
+                    blurRadius: 3,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Find Friend Button
+                  Column(
+                    children: [
+                      Icon(Icons.person_add_alt_1, color: Colors.white),
+                      SizedBox(height: 5),
+                      Text("Find friend",
+                          style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                  SizedBox(width: 100),
+                  // Share Button
+                  Column(
+                    children: [
+                      Icon(Icons.share, color: Colors.white),
+                      SizedBox(height: 5),
+                      Text("Share", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Find Friend Button
-                Column(
-                  children: [
-                    Icon(Icons.person_add_alt_1, color: Colors.white),
-                    SizedBox(height: 5),
-                    Text("Find friend", style: TextStyle(color: Colors.white)),
-                  ],
-                ),
-                SizedBox(width: 100),
-                // Share Button
-                Column(
-                  children: [
-                    Icon(Icons.share, color: Colors.white),
-                    SizedBox(height: 5),
-                    Text("Share", style: TextStyle(color: Colors.white)),
-                  ],
-                ),
-              ],
-            ),
-          ),
 
-          SizedBox(height: 20),
+            SizedBox(height: 20),
 
-          // Mostly Played Section
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Mostly played",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-              ],
+            // Mostly Played Section
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Mostly played",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              children: [
-                MusicTile(
-                  title: "Dekat Di Hati",
-                  artist: "RAN",
-                  imageUrl: "https://via.placeholder.com/50",
-                ),
-                MusicTile(
-                  title: "Bigger Than The Whole Sky",
-                  artist: "Taylor Swift",
-                  imageUrl: "https://via.placeholder.com/50",
-                ),
-                MusicTile(
-                  title: "Matilda",
-                  artist: "Harry Styles",
-                  imageUrl: "https://via.placeholder.com/50",
-                ),
-              ],
+            SizedBox(
+              height: 300,
+              child: ListView(
+                shrinkWrap: true, // Prevent ListView from taking infinite height // Disable ListView's scrolling
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                children: [
+                  MusicTile(
+                    title: "Dekat Di Hati",
+                    artist: "RAN",
+                    imageUrl: "https://via.placeholder.com/50",
+                  ),
+                  MusicTile(
+                    title: "Bigger Than The Whole Sky",
+                    artist: "Taylor Swift",
+                    imageUrl: "https://via.placeholder.com/50",
+                  ),
+                  MusicTile(
+                    title: "Matilda",
+                    artist: "Harry Styles",
+                    imageUrl: "https://via.placeholder.com/50",
+                  ),
+                  MusicTile(
+                    title: "Matilda",
+                    artist: "Harry Styles",
+                    imageUrl: "https://via.placeholder.com/50",
+                  ),
+                  MusicTile(
+                    title: "Matilda",
+                    artist: "Harry Styles",
+                    imageUrl: "https://via.placeholder.com/50",
+                  ),
+                  MusicTile(
+                    title: "Matilda",
+                    artist: "Harry Styles",
+                    imageUrl: "https://via.placeholder.com/50",
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
