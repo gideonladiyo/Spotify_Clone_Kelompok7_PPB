272be8de60ea2eaa303f9363dae2312e62f4d229
Gideon Ladiyo <alwaysfun0604@gmail.com>
2024-12-05 20:00:05 +0700

    create classes

---
+++ lib/design_system/constant/list_item.dart
@@ -0,0 +1,12 @@
+List listIdTracks = [
+  '7t8R5hCOyOKOhEKnbziUgg',
+  '0uTJacQiXYN5EZqBCoSCOz',
+  '3i0h88q1HBtbdIg2bPxs54',
+  '6l1eMKELiSV2L49CNN2aDy',
+  '2FML7gk7ac6quGFIjvkDb3',
+  '1csKczcJqOSdK8JriLMpDW',
+  '7vARkAvliy40kkanvCQvSR',
+  '7st3cap09aLwD2xKVZq9mz',
+  '2NhEUsfmHaEmcSNnNStmAZ',
+  
+];
