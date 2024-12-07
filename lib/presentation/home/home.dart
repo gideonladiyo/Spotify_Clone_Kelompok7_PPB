7d4efd6617e09ad0d237ba9e1b5ad15f0100e0a5
Gideon Ladiyo <alwaysfun0604@gmail.com>
2024-12-07 22:27:45 +0700

    tes

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -5,6 +5,7 @@ import 'package:spotify_group7/presentation/history/history.dart';
 import 'package:spotify_group7/presentation/home/home_page.dart';
 import 'package:spotify_group7/presentation/playlist/playlist.dart';
 import 'package:spotify_group7/presentation/profile/profile.dart';
+import 'package:spotify_group7/presentation/search/search_page.dart';
 // import 'package:spotify_group7/common/widget/bottomNav.dart/bottomNav.dart';
 
 class Home extends StatelessWidget {
@@ -15,6 +16,7 @@ class Home extends StatelessWidget {
     return SafeArea(
       child: BottomNavbar(
           home: HomePage(),
+          search: SearchPage(),
           playlist: Playlist(),
           profile: Profile()),
     );
